4880e2e73caa515a03842b774535e53caadcdc82
Avi <typedvar@users.noreply.github.com>
2015-06-11 21:22:20 +0530

    Update hashfunc.h

--- fibheap_driver/include/hashfunc.h
+++ fibheap_driver/include/hashfunc.h
@@ -1,8 +1,5 @@
 /*
  * author     : avinandan sengupta
- * reg no     : 2010CSY7543
- * course     : CSL630
- * assignment : Fibonacci heap
  * file       : hashfunc.h
  * purpose    : hash function declarations
  */
