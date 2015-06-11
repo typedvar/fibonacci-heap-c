b8cac87822661a839af1e7b486cdd4235579ef0b
Avi <typedvar@users.noreply.github.com>
2015-06-11 21:18:52 +0530

    update header

--- fibheap_lib/src/fibheap.c
+++ fibheap_lib/src/fibheap.c
@@ -1,8 +1,5 @@
 /*
  * author     : avinandan sengupta
- * reg no     : 2010CSY7543
- * course     : CSL630
- * assignment : Fibonacci heap
  * file       : fibheap.c
  * purpose    : Fibonacci heap implementation core
  */
